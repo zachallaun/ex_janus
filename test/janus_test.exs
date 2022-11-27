9c1b8b5d0b99f2dc5a54d0bea2faf99ac681bbb1
Zach Allaun <zach.allaun@gmail.com>
2022-11-27 14:10:04 -0500

    test: test precedence of allow, forbid, and always_allow

--- test/janus_test.exs
+++ test/janus_test.exs
@@ -11,32 +11,45 @@ defmodule JanusTest do
       @impl true
       def policy_for(policy \\ %Janus.Policy{}, actor)
 
-      def policy_for(policy, :basic_read) do
+      def policy_for(policy, :can_read) do
         policy
         |> allow(:read, Thread)
         |> allow(:read, Post)
       end
 
-      def policy_for(policy, :basic_forbid) do
+      def policy_for(policy, :forbid_read) do
         policy
         |> allow(:read, Post)
         |> forbid(:read, Post)
       end
+
+      def policy_for(policy, :override_forbid) do
+        policy
+        |> allow(:read, Post)
+        |> forbid(:read, Post)
+        |> always_allow(:read, Post)
+      end
     end
 
-    test ":basic_read" do
+    test ":can_read" do
       post = post_fixture()
 
-      assert BasicGuestPolicy.allows?(:basic_read, :read, post)
-      assert BasicGuestPolicy.allows?(:basic_read, :read, post.thread)
-      assert BasicGuestPolicy.forbids?(:basic_read, :update, post)
-      assert BasicGuestPolicy.forbids?(:basic_read, :read, post.author)
+      assert BasicGuestPolicy.allows?(:can_read, :read, post)
+      assert BasicGuestPolicy.allows?(:can_read, :read, post.thread)
+      assert BasicGuestPolicy.forbids?(:can_read, :update, post)
+      assert BasicGuestPolicy.forbids?(:can_read, :read, post.author)
     end
 
-    test ":basic_forbid" do
+    test ":forbid_read" do
       post = post_fixture()
 
-      assert BasicGuestPolicy.forbids?(:basic_forbid, :read, post)
+      assert BasicGuestPolicy.forbids?(:forbid_read, :read, post)
+    end
+
+    test ":override_forbid" do
+      post = post_fixture()
+
+      assert BasicGuestPolicy.allows?(:override_forbid, :read, post)
     end
   end
 end
