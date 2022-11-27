44cc96828d2d9b53140b38f7b7e137e3da655406
Zach Allaun <zach.allaun@gmail.com>
2022-11-27 18:28:48 -0500

    refactor: remove unused action argument when running rules

--- lib/janus.ex
+++ lib/janus.ex
@@ -49,9 +49,9 @@ defmodule Janus do
     rule = Janus.Policy.rule_for(policy, action, schema)
 
     false
-    |> allow_if_any?(rule.allow, policy, action, object)
-    |> forbid_if_any?(rule.forbid, policy, action, object)
-    |> allow_if_any?(rule.always_allow, policy, action, object)
+    |> allow_if_any?(rule.allow, policy, object)
+    |> forbid_if_any?(rule.forbid, policy, object)
+    |> allow_if_any?(rule.always_allow, policy, object)
   end
 
   @doc "Returns `true` if the given `policy` forbids `action` to be taken on `object`."
@@ -59,41 +59,41 @@ defmodule Janus do
     !allows?(policy, action, object)
   end
 
-  defp allow_if_any?(true, _conditions, _policy, _action, _object), do: true
+  defp allow_if_any?(true, _conditions, _policy, _object), do: true
 
-  defp allow_if_any?(_, conditions, policy, action, object) do
-    Enum.any?(conditions, &condition_match?(&1, policy, action, object))
+  defp allow_if_any?(_, conditions, policy, object) do
+    Enum.any?(conditions, &condition_match?(&1, policy, object))
   end
 
-  defp forbid_if_any?(false, _conditions, _policy, _action, _object), do: false
+  defp forbid_if_any?(false, _conditions, _policy, _object), do: false
 
-  defp forbid_if_any?(_, conditions, policy, action, object) do
-    !Enum.any?(conditions, &condition_match?(&1, policy, action, object))
+  defp forbid_if_any?(_, conditions, policy, object) do
+    !Enum.any?(conditions, &condition_match?(&1, policy, object))
   end
 
-  defp condition_match?([], _policy, _action, _object), do: true
+  defp condition_match?([], _policy, _object), do: true
 
-  defp condition_match?(condition, policy, action, object) when is_list(condition) do
-    Enum.all?(condition, &condition_match?(&1, policy, action, object))
+  defp condition_match?(condition, policy, object) when is_list(condition) do
+    Enum.all?(condition, &condition_match?(&1, policy, object))
   end
 
-  defp condition_match?({:where, clause}, policy, action, object) do
-    clause_match?(clause, policy, action, object)
+  defp condition_match?({:where, clause}, policy, object) do
+    clause_match?(clause, policy, object)
   end
 
-  defp condition_match?({:where_not, clause}, policy, action, object) do
-    !clause_match?(clause, policy, action, object)
+  defp condition_match?({:where_not, clause}, policy, object) do
+    !clause_match?(clause, policy, object)
   end
 
-  defp clause_match?(list, policy, action, object) when is_list(list) do
-    Enum.all?(list, &clause_match?(&1, policy, action, object))
+  defp clause_match?(list, policy, object) when is_list(list) do
+    Enum.all?(list, &clause_match?(&1, policy, object))
   end
 
-  defp clause_match?({:__janus_derived__, action}, policy, _action, object) do
+  defp clause_match?({:__janus_derived__, action}, policy, object) do
     allows?(policy, action, object)
   end
 
-  defp clause_match?({attr, value}, _policy, _action, object) do
+  defp clause_match?({attr, value}, _policy, object) do
     Map.get(object, attr) == value
   end
 
@@ -108,75 +108,72 @@ defmodule Janus do
 
     {query, where} =
       {from(rule.schema, as: ^@as_ref), false}
-      |> or_where(rule.allow, schema, action, policy)
-      |> and_where_not(rule.forbid, schema, action, policy)
-      |> or_where(rule.always_allow, schema, action, policy)
+      |> or_where(rule.allow, schema, policy)
+      |> and_where_not(rule.forbid, schema, policy)
+      |> or_where(rule.always_allow, schema, policy)
 
     from(query, where: ^where)
   end
 
-  defp or_where({query, where}, [condition | rest], schema, action, policy) do
-    {query, where_condition} = apply_condition(query, condition, schema, action, policy)
+  defp or_where({query, where}, [condition | rest], schema, policy) do
+    {query, where_condition} = apply_condition(query, condition, schema, policy)
 
     {query, simplify_or(where, where_condition)}
-    |> or_where(rest, schema, action, policy)
+    |> or_where(rest, schema, policy)
   end
 
-  defp or_where(acc, [], _schema, _action, _policy), do: acc
+  defp or_where(acc, [], _schema, _policy), do: acc
 
-  defp and_where_not({query, where}, [condition | rest], schema, action, policy) do
-    {query, where_condition} = apply_condition(query, condition, schema, action, policy)
+  defp and_where_not({query, where}, [condition | rest], schema, policy) do
+    {query, where_condition} = apply_condition(query, condition, schema, policy)
 
-    {query, simplify_and(where, dynamic(not ^where_condition))}
-    |> and_where_not(rest, schema, action, policy)
+    {query, simplify_and(where, dynamic(not (^where_condition)))}
+    |> and_where_not(rest, schema, policy)
   end
 
-  defp and_where_not(acc, [], _schema, _action, _policy), do: acc
+  defp and_where_not(acc, [], _schema, _policy), do: acc
 
-  defp apply_condition(query, [], _schema, _action, _policy) do
+  defp apply_condition(query, [], _schema, _policy) do
     {query, true}
   end
 
-  defp apply_condition(query, [{:where, clause} | rest], schema, action, policy) do
-    {query, where_clause, rest_where} =
-      apply_clause_and_rest(query, clause, rest, schema, action, policy)
+  defp apply_condition(query, [{:where, clause} | rest], schema, policy) do
+    {query, where_clause, rest_where} = apply_clause_and_rest(query, clause, rest, schema, policy)
 
     {query, simplify_and(where_clause, rest_where)}
   end
 
-  defp apply_condition(query, [{:where_not, clause} | rest], schema, action, policy) do
-    {query, where_clause, rest_where} =
-      apply_clause_and_rest(query, clause, rest, schema, action, policy)
+  defp apply_condition(query, [{:where_not, clause} | rest], schema, policy) do
+    {query, where_clause, rest_where} = apply_clause_and_rest(query, clause, rest, schema, policy)
 
     {query, simplify_and(dynamic(not (^where_clause)), rest_where)}
   end
 
-  defp apply_clause_and_rest(query, clause, rest_conditions, schema, action, policy) do
-    {query, where_clause} = apply_clause(query, clause, schema, action, policy)
-    {query, rest_where} = apply_condition(query, rest_conditions, schema, action, policy)
+  defp apply_clause_and_rest(query, clause, rest_conditions, schema, policy) do
+    {query, where_clause} = apply_clause(query, clause, schema, policy)
+    {query, rest_where} = apply_condition(query, rest_conditions, schema, policy)
 
     {query, where_clause, rest_where}
   end
 
-  defp apply_clause(query, [clause | rest], schema, action, policy) do
-    {query, where_clause} = apply_clause(query, clause, schema, action, policy)
-    {query, rest_clause} = apply_clause(query, rest, schema, action, policy)
+  defp apply_clause(query, [clause | rest], schema, policy) do
+    {query, where_clause} = apply_clause(query, clause, schema, policy)
+    {query, rest_clause} = apply_clause(query, rest, schema, policy)
 
     {query, simplify_and(where_clause, rest_clause)}
   end
 
-  defp apply_clause(query, [], _schema, _action, _policy), do: {query, nil}
+  defp apply_clause(query, [], _schema, _policy), do: {query, nil}
 
-  defp apply_clause(query, {:__janus_derived__, action}, schema, _action, policy) do
+  defp apply_clause(query, {:__janus_derived__, action}, schema, policy) do
     subquery = filter(schema, action, policy)
 
-    query =
-      from(query, join: sub in subquery(subquery), on: as(^@as_ref).id == sub.id)
+    query = from(query, join: sub in subquery(subquery), on: as(^@as_ref).id == sub.id)
 
     {query, nil}
   end
 
-  defp apply_clause(query, {attr, value}, _schema, _action, _policy) do
+  defp apply_clause(query, {attr, value}, _schema, _policy) do
     {query, dynamic(field(as(^@as_ref), ^attr) == ^value)}
   end
 
