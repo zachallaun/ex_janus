31453535437a8841c4bd53169094aa5d01e0cc80
Zach Allaun <zach.allaun@gmail.com>
2022-12-04 17:31:20 -0500

    feat: mix task to generate basic policy module with helpers

---
+++ lib/janus/mix/tasks/janus.gen.policy.ex
@@ -0,0 +1,86 @@
+defmodule Mix.Tasks.Janus.Gen.Policy do
+  @shortdoc "Generates a basic policy module"
+  @moduledoc """
+  Creates a basic policy module.
+
+      $ mix janus.gen.policy [--module MODULE] [--path PATH]
+
+  Creates the following file:
+
+    * `lib/app_name/policy.ex` - A `Janus.Policy` module containing basic resource
+      loading and authorization helpers
+
+  ## Options
+
+    * `--module` - The name of the generated module, defaults to `AppName.Policy`
+
+    * `--path` - The path (including filename) for the generated module, defaults to
+      `lib/app_name/policy.ex`
+  """
+
+  use Mix.Task
+
+  @doc false
+  def run(args) do
+    opts = parse_args(args)
+
+    copy_from("priv/templates", opts, [
+      {"policy.ex.eex", opts[:path]}
+    ])
+  end
+
+  defp copy_from(source_dir, binding, mapping) do
+    for {source_file, target} <- mapping do
+      source = Path.join(source_dir, source_file)
+      Mix.Generator.create_file(target, EEx.eval_file(source, binding))
+    end
+  end
+
+  defp parse_args(args) do
+    args
+    |> OptionParser.parse!(strict: [module: :string, path: :string])
+    |> Keyword.put_new_lazy(:module, &default_module/0)
+    |> Keyword.update!(:module, &parse_module!/1)
+    |> Keyword.put_new_lazy(:path, &default_path/0)
+    |> Keyword.put_new_lazy(:app_namespace, &app_namespace/0)
+  end
+
+  defp default_path do
+    "lib/#{otp_app()}/policy.ex"
+  end
+
+  defp default_module do
+    Module.concat(app_namespace(), Policy)
+  end
+
+  defp parse_module!(module) when is_atom(module) do
+    ensure_unused!(module)
+  end
+
+  defp parse_module!(module) when is_binary(module) do
+    module
+    |> String.split(".")
+    |> Module.concat()
+    |> ensure_unused!()
+  end
+
+  defp ensure_unused!(module) do
+    name = Module.concat(Elixir, module)
+
+    if Code.ensure_loaded?(name) do
+      Mix.raise(
+        "Module name #{inspect(module)} is already taken. Please specify another using --module."
+      )
+    else
+      module
+    end
+  end
+
+  defp app_namespace do
+    otp_app() |> to_string() |> Macro.camelize()
+  end
+
+  defp otp_app do
+    Mix.Project.config() |> Keyword.fetch!(:app)
+  end
+end
