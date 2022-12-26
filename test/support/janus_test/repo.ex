92dac8311468b149d2094083cbef911a0a292c36
Zach Allaun <zach.allaun@gmail.com>
2022-12-25 19:23:47 -0500

    chore: remove duplicated config options

--- test/support/janus_test/repo.ex
+++ test/support/janus_test/repo.ex
@@ -1,10 +1,5 @@
 defmodule JanusTest.Repo do
   use Ecto.Repo,
     otp_app: :ex_janus,
-    adapter: Ecto.Adapters.Postgres,
-    pool: Ecto.Adapters.SQL.Sandbox,
-    database: "janus_test",
-    username: "dev",
-    password: "dev",
-    hostname: "localhost"
+    adapter: Ecto.Adapters.Postgres
 end
